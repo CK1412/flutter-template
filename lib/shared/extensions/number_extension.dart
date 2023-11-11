3cb5db40dd83c0c93da81071820d160bdc48e5fb
canhth <trancanh1201@gmail.com>
2023-11-11 18:30:23 +0700

    add num extension

---
+++ lib/shared/extensions/number_extension.dart
@@ -0,0 +1,53 @@
+extension NumExtension on num {
+  num plus(num other) {
+    return this + other;
+  }
+
+  num minus(num other) {
+    return this - other;
+  }
+
+  num times(num other) {
+    return this * other;
+  }
+
+  num divide(num other) {
+    return this / other;
+  }
+}
+
+extension IntExtension on int {
+  int plus(int other) {
+    return this + other;
+  }
+
+  int minus(int other) {
+    return this - other;
+  }
+
+  int times(int other) {
+    return this * other;
+  }
+
+  double divide(int other) {
+    return this / other;
+  }
+}
+
+extension DoubleExtension on double {
+  double plus(double other) {
+    return this + other;
+  }
+
+  double minus(double other) {
+    return this - other;
+  }
+
+  double times(double other) {
+    return this * other;
+  }
+
+  double divide(double other) {
+    return this / other;
+  }
+}
