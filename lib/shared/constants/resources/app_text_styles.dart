1e4d39bdba47b2345d6b4eb42bc7d749f7b1cde2
canhth <trancanh1201@gmail.com>
2023-11-12 16:45:56 +0700

    setup dimension

--- lib/shared/constants/resources/app_text_styles.dart
+++ lib/shared/constants/resources/app_text_styles.dart
@@ -6,97 +6,97 @@ import 'app_font_sizes.dart';
 class AppTextStyles {
   AppTextStyles._();
 
-  static final robotoLight10 = TextStyle(
+  static final TextStyle robotoLight10 = TextStyle(
     fontFamily: FontFamily.roboto,
     fontWeight: FontWeight.w300,
     fontSize: AppFontSizes.fontSize10.getRelativeFontSize(),
   );
 
-  static final robotoLight12 = TextStyle(
+  static final TextStyle robotoLight12 = TextStyle(
     fontFamily: FontFamily.roboto,
     fontWeight: FontWeight.w300,
     fontSize: AppFontSizes.fontSize12.getRelativeFontSize(),
   );
 
-  static final robotoLight14 = TextStyle(
+  static final TextStyle robotoLight14 = TextStyle(
     fontFamily: FontFamily.roboto,
     fontWeight: FontWeight.w300,
     fontSize: AppFontSizes.fontSize14.getRelativeFontSize(),
   );
 
-  static final robotoRegular10 = TextStyle(
+  static final TextStyle robotoRegular10 = TextStyle(
     fontFamily: FontFamily.roboto,
     fontWeight: FontWeight.w400,
     fontSize: AppFontSizes.fontSize10.getRelativeFontSize(),
   );
 
-  static final robotoRegular12 = TextStyle(
+  static final TextStyle robotoRegular12 = TextStyle(
     fontFamily: FontFamily.roboto,
     fontWeight: FontWeight.w400,
     fontSize: AppFontSizes.fontSize12.getRelativeFontSize(),
   );
 
-  static final robotoRegular14 = TextStyle(
+  static final TextStyle robotoRegular14 = TextStyle(
     fontFamily: FontFamily.roboto,
     fontWeight: FontWeight.w400,
     fontSize: AppFontSizes.fontSize14.getRelativeFontSize(),
   );
 
-  static final robotoRegular16 = TextStyle(
+  static final TextStyle robotoRegular16 = TextStyle(
     fontFamily: FontFamily.roboto,
     fontWeight: FontWeight.w400,
     fontSize: AppFontSizes.fontSize16.getRelativeFontSize(),
   );
 
-  static final robotoRegular24 = TextStyle(
+  static final TextStyle robotoRegular24 = TextStyle(
     fontFamily: FontFamily.roboto,
     fontWeight: FontWeight.w400,
     fontSize: AppFontSizes.fontSize24.getRelativeFontSize(),
   );
 
-  static final robotoMedium14 = TextStyle(
+  static final TextStyle robotoMedium14 = TextStyle(
     fontFamily: FontFamily.roboto,
     fontWeight: FontWeight.w500,
     fontSize: AppFontSizes.fontSize14.getRelativeFontSize(),
   );
 
-  static final robotoMedium16 = TextStyle(
+  static final TextStyle robotoMedium16 = TextStyle(
     fontFamily: FontFamily.roboto,
     fontWeight: FontWeight.w500,
     fontSize: AppFontSizes.fontSize16.getRelativeFontSize(),
   );
 
-  static final robotoMedium18 = TextStyle(
+  static final TextStyle robotoMedium18 = TextStyle(
     fontFamily: FontFamily.roboto,
     fontWeight: FontWeight.w500,
     fontSize: AppFontSizes.fontSize18.getRelativeFontSize(),
   );
 
-  static final robotoMedium24 = TextStyle(
+  static final TextStyle robotoMedium24 = TextStyle(
     fontFamily: FontFamily.roboto,
     fontWeight: FontWeight.w500,
     fontSize: AppFontSizes.fontSize24.getRelativeFontSize(),
   );
 
-  static final robotoBold14 = TextStyle(
+  static final TextStyle robotoBold14 = TextStyle(
     fontFamily: FontFamily.roboto,
     fontWeight: FontWeight.w700,
     fontSize: AppFontSizes.fontSize14.getRelativeFontSize(),
   );
 
-  static final robotoBold16 = TextStyle(
+  static final TextStyle robotoBold16 = TextStyle(
     fontFamily: FontFamily.roboto,
     fontWeight: FontWeight.w700,
     fontSize: AppFontSizes.fontSize16.getRelativeFontSize(),
   );
 
-  static final robotoBold18 = TextStyle(
+  static final TextStyle robotoBold18 = TextStyle(
     fontFamily: FontFamily.roboto,
     fontWeight: FontWeight.w700,
     fontSize: AppFontSizes.fontSize18.getRelativeFontSize(),
   );
 
-  static final robotoBold24 = TextStyle(
+  static final TextStyle robotoBold24 = TextStyle(
     fontFamily: FontFamily.roboto,
     fontWeight: FontWeight.w700,
     fontSize: AppFontSizes.fontSize24.getRelativeFontSize(),
