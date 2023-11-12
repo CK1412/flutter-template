10e3a94025fee3f41c6ade592c5cb8489f1dacaa
canhth <trancanh1201@gmail.com>
2023-11-12 16:11:55 +0700

    setup text style

---
+++ lib/shared/constants/constants.dart
@@ -0,0 +1,3 @@
+class Constants {
+  Constants._();
+}
---
+++ lib/shared/constants/resources/app_text_styles.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/cupertino.dart';
+
+import '../../../gen/fonts.gen.dart';
+import 'app_font_sizes.dart';
+
+class AppTextStyles {
+  AppTextStyles._();
+
+  static final robotoLight10 = TextStyle(
+    fontFamily: FontFamily.roboto,
+    fontWeight: FontWeight.w300,
+    fontSize: AppFontSizes.fontSize10.getRelativeFontSize(),
+  );
+
+  static final robotoLight12 = TextStyle(
+    fontFamily: FontFamily.roboto,
+    fontWeight: FontWeight.w300,
+    fontSize: AppFontSizes.fontSize12.getRelativeFontSize(),
+  );
+
+  static final robotoLight14 = TextStyle(
+    fontFamily: FontFamily.roboto,
+    fontWeight: FontWeight.w300,
+    fontSize: AppFontSizes.fontSize14.getRelativeFontSize(),
+  );
+
+  static final robotoRegular10 = TextStyle(
+    fontFamily: FontFamily.roboto,
+    fontWeight: FontWeight.w400,
+    fontSize: AppFontSizes.fontSize10.getRelativeFontSize(),
+  );
+
+  static final robotoRegular12 = TextStyle(
+    fontFamily: FontFamily.roboto,
+    fontWeight: FontWeight.w400,
+    fontSize: AppFontSizes.fontSize12.getRelativeFontSize(),
+  );
+
+  static final robotoRegular14 = TextStyle(
+    fontFamily: FontFamily.roboto,
+    fontWeight: FontWeight.w400,
+    fontSize: AppFontSizes.fontSize14.getRelativeFontSize(),
+  );
+
+  static final robotoRegular16 = TextStyle(
+    fontFamily: FontFamily.roboto,
+    fontWeight: FontWeight.w400,
+    fontSize: AppFontSizes.fontSize16.getRelativeFontSize(),
+  );
+
+  static final robotoRegular24 = TextStyle(
+    fontFamily: FontFamily.roboto,
+    fontWeight: FontWeight.w400,
+    fontSize: AppFontSizes.fontSize24.getRelativeFontSize(),
+  );
+
+  static final robotoMedium14 = TextStyle(
+    fontFamily: FontFamily.roboto,
+    fontWeight: FontWeight.w500,
+    fontSize: AppFontSizes.fontSize14.getRelativeFontSize(),
+  );
+
+  static final robotoMedium16 = TextStyle(
+    fontFamily: FontFamily.roboto,
+    fontWeight: FontWeight.w500,
+    fontSize: AppFontSizes.fontSize16.getRelativeFontSize(),
+  );
+
+  static final robotoMedium18 = TextStyle(
+    fontFamily: FontFamily.roboto,
+    fontWeight: FontWeight.w500,
+    fontSize: AppFontSizes.fontSize18.getRelativeFontSize(),
+  );
+
+  static final robotoMedium24 = TextStyle(
+    fontFamily: FontFamily.roboto,
+    fontWeight: FontWeight.w500,
+    fontSize: AppFontSizes.fontSize24.getRelativeFontSize(),
+  );
+
+  static final robotoBold14 = TextStyle(
+    fontFamily: FontFamily.roboto,
+    fontWeight: FontWeight.w700,
+    fontSize: AppFontSizes.fontSize14.getRelativeFontSize(),
+  );
+
+  static final robotoBold16 = TextStyle(
+    fontFamily: FontFamily.roboto,
+    fontWeight: FontWeight.w700,
+    fontSize: AppFontSizes.fontSize16.getRelativeFontSize(),
+  );
+
+  static final robotoBold18 = TextStyle(
+    fontFamily: FontFamily.roboto,
+    fontWeight: FontWeight.w700,
+    fontSize: AppFontSizes.fontSize18.getRelativeFontSize(),
+  );
+
+  static final robotoBold24 = TextStyle(
+    fontFamily: FontFamily.roboto,
+    fontWeight: FontWeight.w700,
+    fontSize: AppFontSizes.fontSize24.getRelativeFontSize(),
+  );
+}
