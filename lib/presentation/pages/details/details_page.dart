c827624d4e7c815822f8ff94493dd23e57a46d7a
canhth <trancanh1201@gmail.com>
2024-01-15 23:03:44 +0700

    example use shimmer loading

--- lib/presentation/pages/details/details_page.dart
+++ lib/presentation/pages/details/details_page.dart
@@ -5,12 +5,20 @@ import '../../../app/navigation/app_navigator.dart';
 import '../../../l10n/generated/l10n.dart';
 import '../../../shared/resources/resources.dart';
 import '../../common_widgets/app_bar/common_app_bar.dart';
-import '../../common_widgets/base/common_scaffold.dart';
+import '../../common_widgets/common_scaffold.dart';
+import '../../common_widgets/shimmer/shimmer_loading.dart';
 
-class DetailsPage extends StatelessWidget {
+class DetailsPage extends StatefulWidget {
   const DetailsPage({super.key, required this.id});
 
   final String id;
+
+  @override
+  State<DetailsPage> createState() => _DetailsPageState();
+}
+
+class _DetailsPageState extends State<DetailsPage> {
+  bool _loading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -22,18 +30,38 @@ class DetailsPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Id: $id'),
+            Text('Id: ${widget.id}'),
             FilledButton(
               onPressed: () {
                 AppNavigator.pop();
               },
               child: const Text('Pop page'),
             ),
-            Container(
-              width: double.maxFinite,
-              color: Colors.amberAccent,
-              height: AppDimens.spacing100,
-              margin: const EdgeInsets.symmetric(horizontal: AppInsets.inset16),
+            FilledButton(
+              onPressed: () {
+                setState(() {
+                  _loading = true;
+                });
+                Future.delayed(
+                  const Duration(seconds: 3),
+                  () {
+                    setState(() {
+                      _loading = false;
+                    });
+                  },
+                );
+              },
+              child: const Text('Test Shimmer loading'),
+            ),
+            ShimmerLoading(
+              isLoading: _loading,
+              child: Container(
+                width: double.maxFinite,
+                color: Colors.amberAccent,
+                height: AppDimens.spacing100,
+                margin:
+                    const EdgeInsets.symmetric(horizontal: AppInsets.inset16),
+              ),
             ),
             const Gap(AppDimens.spacing1000),
           ],
